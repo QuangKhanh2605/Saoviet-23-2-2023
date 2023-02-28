c6b64300fcbc1f869245b2fa20be3dab78b08fda
QuangKhanh2605 <125939258+QuangKhanh2605@users.noreply.github.com>
2023-02-28 08:20:08 +0700

    update 8h20_28/2

--- Demo_4/Demo_4/Drivers/Mylib_driver/Inc/check_Button.h
+++ Demo_4/Demo_4/Drivers/Mylib_driver/Inc/check_Button.h
@@ -9,6 +9,7 @@ void HAL_SYSTICK_Callback(void);
 
 void press_Click_BT_Up(uint16_t *BT_UP, uint16_t *ptr_stamp);
 void press_Click_BT_Down(uint16_t *BT_Down, uint16_t *ptr_stamp);
+
 void press_Hold_BT_Up( GPIO_TypeDef* GPIOx, uint16_t GPIO_Pinx, uint16_t *ptr_stamp);
 void press_Hold_BT_DOWN( GPIO_TypeDef* GPIOx, uint16_t GPIO_Pinx, uint16_t *ptr_stamp);
 
